a3a51b80c4b185f3edcf10d4e8c00c376e79353a
DaniloMV <danilo7valladarez@gmail.com>
2021-05-01 18:39:32 -0500

    Inicio Instalación paquetes EF

---
+++ DemoEFCoreWindowsForms/Models/ApplicationDbContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoEFCoreWindowsForms.Models
+{
+
+	///Mi configuración de Entity Framework Core 3.1:
+
+	public class ApplicationDbContext : DbContext
+	{
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer("Data Source=(localdb)\\mssqllocaldb;Initial Catalog=DemoWinForms2;Integrated Security=True")
+				.EnableSensitiveDataLogging(true)
+				.UseLoggerFactory(MyLoggerFactory);
+		}
+
+		public static readonly ILoggerFactory MyLoggerFactory = LoggerFactory.Create(builder =>
+		{
+			builder
+			   .AddFilter((category, level) =>
+				   category == DbLoggerCategory.Database.Command.Name
+				   && level == LogLevel.Information)
+			   .AddConsole();
+		});
+
+		public DbSet<Student> Students { get; set; }
+
+
+	}
+}
